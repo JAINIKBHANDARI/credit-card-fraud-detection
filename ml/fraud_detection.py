577addac2fa6387a55fe4bf3571f43eec9432f0c
Harshrana048 <harsh29011@gmail.com>
2026-01-19 14:55:55 +0530

    Add project structure and ML code

---
+++ ml/fraud_detection.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("../data/creditcard.csv")
+
+# print("First 5 rows of the dataset:")
+# print(df.head())
+
+# print("\nDataset Shape:")
+# print(df.shape)
+
+# print("\nClass Distribution:")
+# print(df['Class'].value_counts())
+# # Dataset information
+# print("\nDataset Info:")
+# print(df.info())
+
+# # Statistical summary
+# print("\nDataset Description:")
+# print(df.describe())
+
+# # Check missing values
+# print("\nMissing values per column:")
+# print(df.isnull().sum())
+
+print(df['Class'].value_counts())
+
+fraud = df[df['Class'] == 1]
+normal = df[df['Class'] == 0]
+
+print(fraud.shape)
+print(normal.shape)

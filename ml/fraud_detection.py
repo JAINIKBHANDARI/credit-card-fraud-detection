b9888b15abf2dbf575e7405d51eba83879da2dcc
Harshrana048 <harsh29011@gmail.com>
2026-01-30 13:33:41 +0530

    Secondcommit

--- ml/fraud_detection.py
+++ ml/fraud_detection.py
@@ -1,26 +1,45 @@
+
+import warnings
+warnings.filterwarnings("ignore")
 import pandas as pd
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 # Load dataset
-df = pd.read_csv("../data/creditcard.csv")
-
-print("First 5 rows of the dataset:")
-print(df.head())
-
-print("\nDataset Shape:")
-print(df.shape)
-
-print("\nClass Distribution:")
-print(df['Class'].value_counts())
-# Dataset information
-print("\nDataset Info:")
-print(df.info())
-
-# Statistical summary
-print("\nDataset Description:")
-print(df.describe())
+df = pd.read_csv("creditcard.csv")
 
 # Check missing values
-print("\nMissing values per column:")
 print(df.isnull().sum())
+
+# Separate features and target
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+# Feature scaling
+scaler = StandardScaler()
+X['Amount'] = scaler.fit_transform(X[['Amount']])
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("Training set size:", X_train.shape)
+print("Testing set size:", X_test.shape)
+
+
+from imblearn.over_sampling import SMOTE
+
+# Apply SMOTE only on training data
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+print("Before SMOTE:\n", y_train.value_counts())
+print("After SMOTE:\n", y_train_resampled.value_counts())
+
+
 
 

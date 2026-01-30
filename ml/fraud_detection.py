1495a4cc406257589b628df156d3c8e88755ae8d
Harshrana048 <harsh29011@gmail.com>
2026-01-30 14:02:29 +0530

    logistic Model train

--- ml/fraud_detection.py
+++ ml/fraud_detection.py
@@ -1,6 +1,7 @@
 
 import warnings
 warnings.filterwarnings("ignore")
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
@@ -9,7 +10,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 # Load dataset
-df = pd.read_csv("creditcard.csv")
+# Load dataset
+df = pd.read_csv("data/creditcard.csv")
+
+
 
 # Check missing values
 print(df.isnull().sum())
@@ -39,6 +43,25 @@ X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
 
 print("Before SMOTE:\n", y_train.value_counts())
 print("After SMOTE:\n", y_train_resampled.value_counts())
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Train Logistic Regression
+lr_model = LogisticRegression(max_iter=3000)
+lr_model.fit(X_train_resampled, y_train_resampled)
+
+# Predict on test data
+y_pred_lr = lr_model.predict(X_test)
+
+# Evaluation
+print("Confusion Matrix (Logistic Regression):")
+print(confusion_matrix(y_test, y_pred_lr))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred_lr))
+
+
 
 
 
